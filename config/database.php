164c0b6f24cb26e9f7129b0e9b3602ac24718b41
luiscflores <luisfloresc95@gmail.com>
2026-07-16 03:07:04 +0100

    fix sqlite mmap size for rpi3

--- config/database.php
+++ config/database.php
@@ -44,7 +44,7 @@ return [
             'pragmas' => [
                 'cache_size' => -128000,
                 'temp_store' => 'MEMORY',
-                'mmap_size' => 268435456,
+                'mmap_size' => 33554432, // 32MB instead of 256MB for RPi3 compatibility
             ],
             'transaction_mode' => 'IMMEDIATE',
         ],
